aec5c7e3c6df7746a3a3fe5e341b3e27e8129d40
sousmiley <soumyaparmar@hotmail.com>
2025-12-04 16:09:58 -0800

    feat : minor changes

--- rootine/src/pages/_app.js
+++ rootine/src/pages/_app.js
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import '@/styles.css';
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
